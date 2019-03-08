768902ec4aa7aa13b8c5d2d464b1b71692cf9e80
FS <48133414+SF1101@users.noreply.github.com>
2019-03-08 10:56:43 +0200

    Felix
    Added Felix

---
+++ Felix.ps1
@@ -0,0 +1,16 @@
+﻿Start-Transcript -Path "C:\Temp\Felix\Ferror.txt"
+
+Set-Audiodevice -index <string>
+Set-AudioDevice -PlaybackMute 0
+Set-AudioDevice -PlaybackVolume 40
+Set-AudioDevice -RecordingVolume 50 
+
+
+stop-Transcript
+$ErrorOccured
+try
+{
+
+Send-MailMessage -Smtpserver "smtp.abo.fi" -From "adminreport@abo.fi" -To "john.sjoholm@abo.fi" -Subject "Error" -Body "Powershell script in <hus> <auditorie> has failed" -Attachments "C:\Temp\Felix\Ferror.txt"
+}
+pause /exit
