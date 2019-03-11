030146b98f64ab688e618dd4635d10d599bbc17d
FS <48133414+SF1101@users.noreply.github.com>
2019-03-11 10:17:10 +0200

    Update kiwi.ps1

--- kiwi.ps1
+++ kiwi.ps1
@@ -1,4 +1,4 @@
-﻿Start-Transcript -Path "C:\Users\fsjoberg\Desktop\index.txt"
+﻿Start-Transcript -Path "$env:userprofile\Desktop\index.txt"
 Get-AudioDevice -list
 Stop-Transcript
-Get-PnpDevice -PresentOnly
+exit
